e7070ccb3a04b31dfa7fa57d45062e12870a29af
snehajadhav113 <sj6275179@gmail.com>
2025-03-24 14:51:06 +0530

    Codealpha task 1

---
+++ script.js
@@ -0,0 +1,24 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.gallery-image');
+
+function showImage(index) {
+  images.forEach((img, i) => {
+    img.classList.remove('active');
+    if (i === index) {
+      img.classList.add('active');
+    }
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+// Initialize the gallery with the first image
+showImage(currentIndex);
